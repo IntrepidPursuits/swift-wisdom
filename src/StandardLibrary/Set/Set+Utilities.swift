e51ed69351944d9fdeb2a241556fdc45d2466b3d
Logan Wright <logan.william.wright@gmail.com>
2015-10-27 14:15:50 -0400

    small updates

--- src/StandardLibrary/Set/Set+Utilities.swift
+++ src/StandardLibrary/Set/Set+Utilities.swift
@@ -1,3 +1,4 @@
+
 public func +=<T>(inout lhs: Set<T>, rhs: Set<T>) {
     lhs = lhs.union(rhs)
 }
@@ -10,7 +11,7 @@ public extension Set {
     func ip_toArray() -> [Element] {
         return Array<Element>(self)
     }
-
+    
     func ip_passesTest(@noescape test: (element: Element) -> Bool) -> Bool {
         for ob in self {
             if test(element: ob) {
@@ -19,18 +20,7 @@ public extension Set {
         }
         return false
     }
-
-    func ip_remove(element: Element) -> Bool {
-        var toSubtract = Set<Element>()
-        for ob in self {
-            if ob == element {
-                toSubtract.insert(ob)
-            }
-        }
-        subtract(toSubtract)
-        return toSubtract.count > 0
-    }
-
+    
     func ip_filter(@noescape include: (element: Element) -> Bool) -> Set<Element> {
         var filtered = Set<Element>()
         for ob in self {
