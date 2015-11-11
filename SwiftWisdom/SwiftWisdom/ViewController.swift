3fa495aca22f94c0770d905ba7ee83dc21454d6d
Logan Wright <logan.william.wright@gmail.com>
2015-11-10 22:33:21 -0500

    adding project

---
+++ SwiftWisdom/SwiftWisdom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftWisdom
+//
+//  Created by Logan Wright on 11/10/15.
+//  Copyright © 2015 Intrepid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
