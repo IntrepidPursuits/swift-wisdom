7ebe3aa21225dcb257032051f9025a12b3538ee8
Logan Wright <logan.william.wright@gmail.com>
2015-12-11 15:54:37 -0500

    adding version class

---
+++ SwiftWisdom/SwiftWisdom/Core/Version/Version.swift
@@ -0,0 +1,81 @@
+//
+//  Version.swift
+//  bmap
+//
+//  Created by Logan Wright on 11/30/15.
+//  Copyright © 2015 Intrepid. All rights reserved.
+//
+
+import Foundation
+
+public struct Version {
+    public private(set) var major: Int = 0
+    public private(set) var minor: Int = 0
+    public private(set) var patch: Int = 0
+    
+    init?(string: String) {
+        let components = string
+            .componentsSeparatedByString(".")
+        guard components.count > 0, let major = Int(components[0]) else { return nil }
+        self.major = major
+        
+        guard components.count > 1, let minor = Int(components[1]) else { return nil }
+        self.minor = minor
+        
+        guard components.count > 2, let patch = Int(components[2]) else { return nil }
+        self.patch = patch
+    }
+}
+
+extension Version : CustomStringConvertible {
+    public var description: String {
+        return "\(major).\(minor).\(patch)"
+    }
+}
+
+extension Version : Equatable {}
+
+public func ==(lhs: Version, rhs: Version) -> Bool {
+    guard lhs.major == rhs.major else { return false }
+    guard lhs.minor == rhs.minor else { return false }
+    guard lhs.patch == rhs.patch else { return false }
+    return true
+}
+
+public func !=(lhs: Version, rhs: Version) -> Bool {
+    return !(lhs == rhs)
+}
+
+extension Version : Comparable {}
+
+public func <(lhs: Version, rhs: Version) -> Bool {
+    if lhs.major < rhs.major {
+        return true
+    } else if lhs.minor < rhs.minor {
+        return true
+    } else if lhs.patch < rhs.patch {
+        return true
+    } else {
+        return false
+    }
+}
+
+public func <=(lhs: Version, rhs: Version) -> Bool {
+    return lhs < rhs || lhs == rhs
+}
+
+public func >=(lhs: Version, rhs: Version) -> Bool {
+    return lhs > rhs || lhs == rhs
+}
+
+public func >(lhs: Version, rhs: Version) -> Bool {
+    if lhs.major > rhs.major {
+        return true
+    } else if lhs.minor > rhs.minor {
+        return true
+    } else if lhs.patch > rhs.patch {
+        return true
+    } else {
+        return false
+    }
+}
