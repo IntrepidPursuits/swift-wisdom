46b6dd531f109860f104eabba4eda1798975ac38
Logan Wright <logan.william.wright@gmail.com>
2015-12-12 00:23:04 -0500

    adding quick function for traditional dispatching

--- SwiftWisdom/SwiftWisdom/Core/Async/After/After.swift
+++ SwiftWisdom/SwiftWisdom/Core/Async/After/After.swift
@@ -7,6 +7,18 @@
 
 import Foundation
 
+// MARK: Now
+
+public func Main(function: Block) {
+    dispatch_async(dispatch_get_main_queue(), function)
+}
+
+public func Background(function: Block) {
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), function)
+}
+
+// MARK: Later
+
 public func After(after: NSTimeInterval, op: () -> ()) {
     After(after, op: op, completion: nil)
 }
