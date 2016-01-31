0f13513030983dc1d3eadadb8fb64e7a35b9f235
Logan Wright <logan.william.wright@gmail.com>
2016-01-30 19:34:58 -0500

    tweaking inter type extensions and adding tests for found bug

---
+++ SwiftWisdomTests/IntegerTypeTests.swift
@@ -0,0 +1,27 @@
+//
+//  IntegerTypeTests.swift
+//  SwiftWisdom
+//
+//  Created by Logan Wright on 1/30/16.
+//  Copyright © 2016 Intrepid. All rights reserved.
+//
+
+import XCTest
+import SwiftWisdom
+
+class IntegerTypeTests: XCTestCase {
+
+    func testEvenOdd() {
+        XCTAssert(32.ip_isEven)
+        XCTAssert(153.ip_isOdd)
+        XCTAssert((-42).ip_isEven)
+        XCTAssert((-1537).ip_isOdd)
+    }
+    
+    func testTimes() {
+        let count = 324
+        var collection: [String] = []
+        count.times { collection.append("Hi") }
+        XCTAssert(count == collection.count)
+    }
+}
