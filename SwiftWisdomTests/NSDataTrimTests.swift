239ddc9ae77c830da60355eb51f393a3dcfdd927
Paul Rolfe <paul.a.rolfe@gmail.com>
2016-10-06 09:51:44 -0400

    isolating trim-range tests.

--- SwiftWisdomTests/NSDataTrimTests.swift
+++ SwiftWisdomTests/NSDataTrimTests.swift
@@ -16,11 +16,11 @@ class NSDataTrimTests: XCTestCase {
         let data = "aa 11 11 11 11 11 bb".ip_dataFromHexadecimalString()!
         let mutable = NSMutableData(data: data as Data)
         mutable.ip_trimRange(0...3)
-        XCTAssert(mutable as NSData == data.ip_suffixFrom(4))
+        XCTAssert(mutable == "11 11 bb".ip_dataFromHexadecimalString()!)
         
         
         let endMutable = NSMutableData(data: data as Data)
         endMutable.ip_trimRange(3...100)
-        XCTAssert(endMutable == data.ip_prefixThrough(2))
+        XCTAssert(endMutable == "aa 11 11".ip_dataFromHexadecimalString()!)
     }
 }
