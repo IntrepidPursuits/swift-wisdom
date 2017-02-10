d9fc6f5b5ca3da147fbcc682deed545cd50ac560
Tom Le <son@intrepid.io>
2017-02-10 10:49:44 -0500

    Update unit tests per PR comments.

--- SwiftWisdomTests/CommonTypes/MulticastTests.swift
+++ SwiftWisdomTests/CommonTypes/MulticastTests.swift
@@ -40,7 +40,35 @@ private class Subscriber: BroadcasterDelegate {
 
 class MulticastTests: XCTestCase {
 
-    func testAddingDelegates() {
+    func testBroadcastShouldBroadcastToAllDelegates() {
+        var messageRecipients = [String]()
+        let subscriberA = Subscriber { message in
+            if message == "We are wonderful" {
+                messageRecipients.append("A")
+            }
+        }
+        let subscriberB = Subscriber { message in
+            if message == "We are wonderful" {
+                messageRecipients.append("B")
+            }
+        }
+        let subscriberC = Subscriber { message in
+            if message == "We are wonderful" {
+                messageRecipients.append("C")
+            }
+        }
+
+        let broadcaster = Broadcaster()
+        broadcaster.add(delegate: subscriberA)
+        broadcaster.add(delegate: subscriberB)
+        broadcaster.add(delegate: subscriberC)
+        broadcaster.broadcast(that: "We are wonderful")
+
+        XCTAssert(messageRecipients.ip_contains(all: ["A", "B", "C"]), "Message not broadcasted to all recipients.")
+        XCTAssert(messageRecipients.count == 3, "Message was broadcasted to too many recipients.")
+    }
+
+    func testAddingSameDelegateShouldNotReadd() {
         var messageRecipients = [String]()
         let subscriberA = Subscriber { message in
             if message == "We are intrepid" {
@@ -64,9 +92,9 @@ class MulticastTests: XCTestCase {
         broadcaster.add(delegate: subscriberC)
         broadcaster.add(delegate: subscriberA)
         broadcaster.broadcast(that: "We are intrepid") // get it?
-        if !messageRecipients.ip_contains(all: ["A", "B", "C"]) || messageRecipients.count != 3 {
-            XCTFail("Intended recipients are A, B, and C. Actual recipients: \(messageRecipients)")
-        }
+
+        XCTAssert(messageRecipients.ip_contains(all: ["A", "B", "C"]), "Message not broadcasted to all recipients.")
+        XCTAssert(messageRecipients.count == 3, "Message was broadcasted to too many recipients.")
     }
 
     func testRemovingOneDelegate() {
@@ -93,9 +121,9 @@ class MulticastTests: XCTestCase {
         broadcaster.add(delegate: subscriberC)
         broadcaster.remove(delegate: subscriberB)
         broadcaster.broadcast(that: "We are beautiful")
-        if !messageRecipients.ip_contains(all: ["A", "C"]) || messageRecipients.count != 2 {
-            XCTFail("Intended recipients are A and C. Actual recipients: \(messageRecipients)")
-        }
+
+        XCTAssert(messageRecipients.ip_contains(all: ["A", "C"]), "Message not broadcasted to all recipients.")
+        XCTAssert(messageRecipients.count == 2, "Message was broadcasted to too many recipients.")
     }
 
     func testRemovingMultipleDelegates() {
@@ -122,8 +150,7 @@ class MulticastTests: XCTestCase {
         broadcaster.add(delegate: subscriberC)
         broadcaster.removeDelegates { $0 === subscriberA || $0 === subscriberC }
         broadcaster.broadcast(that: "We are gorgeous")
-        if messageRecipients != ["B"] {
-            XCTFail("Intended recipient is B. Actual recipients: \(messageRecipients)")
-        }
+
+        XCTAssert(messageRecipients == ["B"], "Message was not broadcasted to the right recipient.")
     }
 }
