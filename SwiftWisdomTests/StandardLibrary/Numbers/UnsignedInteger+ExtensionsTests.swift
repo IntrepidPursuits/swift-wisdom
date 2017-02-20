280b1d8234b2adb64f8d10e25f42b52818142e40
Colden Prime <colden@intrepid.io>
2017-02-20 15:49:25 -0500

    Convert between (Int|UInt)(16|32|64) and Data while respecting endianness

--- SwiftWisdomTests/StandardLibrary/Numbers/UnsignedInteger+ExtensionsTests.swift
+++ SwiftWisdomTests/StandardLibrary/Numbers/UnsignedInteger+ExtensionsTests.swift
@@ -19,6 +19,7 @@ class UnsignedIntegerExtensionTests: XCTestCase {
         let mixed = -100...100
         XCTAssert(mixed.contains(random(inRange: mixed)))
     }
+
     func test_bitStackOfLength() {
         XCTAssertEqual(UInt8.ip_bitStackOfLength(-1), UInt8(0))
         XCTAssertEqual(UInt8.ip_bitStackOfLength(0), UInt8(1 - 1)) // 2^0 - 1
@@ -29,5 +30,23 @@ class UnsignedIntegerExtensionTests: XCTestCase {
         // overflow just returns the max number of 1s for that type
         XCTAssertEqual(UInt8.ip_bitStackOfLength(9), UInt8(256 - 1)) // 2^8 - 1
         XCTAssertEqual(UInt16.ip_bitStackOfLength(16), UInt16(65536 - 1)) // 2^16 - 1
+    }
+
+    func testUInt16EndianConversion() {
+        let value: UInt16 = 1
+        XCTAssertEqual(Data(bytes: [0x00, 0x01]), value.ip_bigEndianData)
+        XCTAssertEqual(Data(bytes: [0x01, 0x00]), value.ip_littleEndianData)
+    }
+
+    func testUInt32EndianConversion() {
+        let value: UInt32 = 1
+        XCTAssertEqual(Data(bytes: [0x00, 0x00, 0x00, 0x01]), value.ip_bigEndianData)
+        XCTAssertEqual(Data(bytes: [0x01, 0x00, 0x00, 0x00]), value.ip_littleEndianData)
+    }
+
+    func testUInt64EndianConversion() {
+        let value: UInt64 = 1
+        XCTAssertEqual(Data(bytes: [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01]), value.ip_bigEndianData)
+        XCTAssertEqual(Data(bytes: [0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]), value.ip_littleEndianData)
     }
 }
