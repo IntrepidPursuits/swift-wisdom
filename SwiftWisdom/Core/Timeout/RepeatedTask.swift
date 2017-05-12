4d2b59c478af3451d30e48252e713d9d02d06b9b
Paul Rolfe <paul.a.rolfe@gmail.com>
2017-05-12 17:25:20 -0400

    RepeatedTask added

---
+++ SwiftWisdom/Core/Timeout/RepeatedTask.swift
@@ -0,0 +1,56 @@
+//
+//  RepeatedTask.swift
+//  SwiftWisdom
+//
+//  Created by Paul Rolfe on 5/12/17.
+//  Copyright © 2017 Intrepid. All rights reserved.
+//
+
+import Foundation
+
+public class RepeatedTask {
+    let operation: Block
+    let timeBetweenOps: TimeInterval
+    /// nil repeatCount means infinite
+    let repeatCount: Int?
+    let queue: DispatchQueue
+
+    private var currentCount: Int = 0
+    private var isComplete: Bool = false
+    private var isActive: Bool = false
+
+    public init(repeatCount: Int? = nil, timeBetweenOps: TimeInterval = 0, on queue: DispatchQueue = DispatchQueue.main, operation: @escaping Block) {
+        self.operation = operation
+        self.timeBetweenOps = timeBetweenOps
+        self.repeatCount = repeatCount
+        self.queue = queue
+        start()
+    }
+
+    public func start() {
+        guard !isActive else { return }
+        isActive = true
+        executeOp()
+    }
+
+    public func stop() {
+        guard isActive else { return }
+        isActive = false
+    }
+
+    private func executeOp() {
+        if let count = repeatCount {
+            guard currentCount < count else {
+                isComplete = true
+                return
+            }
+        }
+        guard isActive else { return }
+
+        operation()
+        currentCount += 1
+        After(timeBetweenOps, on: queue) { [weak self] in
+            self?.executeOp()
+        }
+    }
+}
